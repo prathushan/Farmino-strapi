007ff0a9999aa4096809a73f46be2f2fbe3ee439
prathushan <119046937+prathushan@users.noreply.github.com>
2025-07-18 18:04:15 +0530

    added new field

---
+++ src/api/rediscover-the-taste/routes/rediscover-the-taste.ts
@@ -0,0 +1,7 @@
+/**
+ * rediscover-the-taste router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::rediscover-the-taste.rediscover-the-taste');
